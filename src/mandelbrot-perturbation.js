f8a0ff5b7b3cf1c48f3eeb9449653ff760a4c0f5
quevivasbien <quevivasbien@gmail.com>
2024-08-01 20:18:20 -0400

    fixed perturbation method

--- src/mandelbrot-perturbation.js
+++ src/mandelbrot-perturbation.js
@@ -21,30 +21,37 @@ export function render(refresh=false) {
 
     // Select base point
     const base_c = {
-        re: x0.plus(new Decimal(baseCoords.x).times(x1.minus(x0))),
-        im: y1.minus(new Decimal(baseCoords.y).times(y1.minus(y0))),
+        re: x0.plus(x1.minus(x0).times(baseCoords.x)),
+        im: y0.plus(y1.minus(y0).times(baseCoords.y)),
     };
     let z0 = base_c;
     const base_z = [{ re: z0.re.toNumber(), im: z0.im.toNumber() }];
     for (let iter = 0; iter < maxIters - 1; iter++) {
         const z_new = {
             re: z0.re.times(z0.re).minus(z0.im.times(z0.im)).plus(base_c.re),
-            im: new Decimal(2.0).times(z0.re).times(z0.im).plus(base_c.im),
+            im: z0.re.times(z0.im).times(2.0).plus(base_c.im),
         };
         z0 = z_new;
         base_z.push({ re: z_new.re.toNumber(), im: z_new.im.toNumber() });
     }
-
+    const width = x1.minus(x0).toNumber();
+    const height = y1.minus(y0).toNumber();
+    
+    console.log("baseCoords:", baseCoords);
     for (let i = 0; i < canvas.height; i++) {
         for (let j = 0; j < canvas.width; j++) {
             const idx = i * canvas.width + j;
-            const x = x0.plus(x1.minus(x0).times(j).div(canvas.width - 1));
-            const y = y1.minus(y1.minus(y0).times(i).div(canvas.height - 1));
-            const c = { re: x, im: y };
-            const dc = { re: c.re.minus(base_c.re).toNumber(), im: c.im.minus(base_c.im).toNumber() };
-            const z = { re: c.re.toNumber(), im: c.im.toNumber() };
+            const xCoord = j / (canvas.width - 1);
+            const yCoord = 1 - i / (canvas.height - 1);
+            const relXCoord = xCoord - baseCoords.x;
+            const relYCoord = yCoord - baseCoords.y;
+            const dx = relXCoord * width;
+            const dy = relYCoord * height;
+            const dc = { re: dx, im: dy };
             const dz = { ...dc };
             for (let iter = 0; iter < maxIters; iter++) {
+                const z0 = base_z[iter];
+                const z = { re: z0.re + dz.re, im: z0.im + dz.im };
                 if (z.re * z.re + z.im * z.im > 4.0) {
                     const t = iter + 1 - Math.log(Math.log(z.re * z.re + z.im * z.im)) / Math.log(2.0);
                     newData[idx * 4 + 0] = 255 * (1.0 - Math.cos(0.02 * t)) / 2.0;
@@ -52,14 +59,11 @@ export function render(refresh=false) {
                     newData[idx * 4 + 2] = 255 * (1.0 - Math.cos(0.05 * t)) / 2.0;
                     break;
                 }
-                const z0 = base_z[iter];
                 // dz' = 2 * z * dz + dz^2 + dc
                 const dz_re_new = 2.0 * z0.re * dz.re - 2.0 * z0.im * dz.im + dz.re * dz.re - dz.im * dz.im + dc.re;
                 const dz_im_new = 2.0 * z0.re * dz.im + 2.0 * z0.im * dz.re + 2.0 * dz.re * dz.im + dc.im;
                 dz.re = dz_re_new;
                 dz.im = dz_im_new;
-                z.re = z0.re + dz.re;
-                z.im = z0.re + dz.im;
             }
             newData[idx * 4 + 3] = 255;
         }
@@ -73,12 +77,14 @@ export function render(refresh=false) {
     // draw an X at the base point
     ctx.strokeStyle = "red";
     ctx.lineWidth = 2;
-    ctx.moveTo((baseCoords.x - 0.005) * canvas.width, (1.0-baseCoords.y + 0.005) * canvas.height);
-    ctx.lineTo((baseCoords.x + 0.005) * canvas.width, (1.0-baseCoords.y - 0.005) * canvas.height);
+    const xCenter = baseCoords.x * canvas.width;
+    const yCenter = (1-baseCoords.y) * canvas.height;
+    ctx.moveTo(xCenter - 5, yCenter + 5);
+    ctx.lineTo(xCenter + 5, yCenter - 5);
     ctx.stroke();
 
-    ctx.moveTo((baseCoords.x - 0.005) * canvas.width, (1.0-baseCoords.y - 0.005) * canvas.height);
-    ctx.lineTo((baseCoords.x + 0.005) * canvas.width, (1.0-baseCoords.y + 0.005) * canvas.height);
+    ctx.moveTo(xCenter - 5, yCenter - 5);
+    ctx.lineTo(xCenter + 5, yCenter + 5);
     ctx.stroke();
 
 }
